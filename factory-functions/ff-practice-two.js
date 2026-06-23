823dc534f4ab07090cd591f8e599567544a47306
Alex Aaron <alexfrancisaaron@gmail.com>
2026-06-23 16:55:36 +0000

    created factory function practice

---
+++ factory-functions/ff-practice-two.js
@@ -0,0 +1,65 @@
+const carOneName = "Kia Rio";
+const carOneTopSpeed = 118;
+const carOneYear = 2021;
+
+const carTwoName = "Toyota Camry";
+const carTwoTopSpeed = 135;
+const carTwoYear = 2026;
+
+/*
+Part 1 - define the createCar factory function below
+Parameters: name (string), topSpeed (a number), year (a number)
+Return an object with keys of name, topSpeed, and year set to the inputs.
+*/
+
+function createCar(){
+
+}
+
+// Part 2 - Declare variables to store two car objects
+// Call your createCar function to create car1 and car2, using the hardcoded variables above
+
+
+
+
+
+// Part 3 - Create two additional car objects
+// Call your createCar function to declare car3 and car4, using your own data
+
+
+
+
+
+// Part 4 - Define the getFastestCar function below
+// Parameters: carA and carB (two objects)
+// Return: the object with the higher topSpeed, or the car that is newest if the topSpeed is equal
+
+
+
+
+// Predefined function - write code for part 5 in this function's code block
+/*
+Part 5:
+- Step one: call getFastestCar to determine fastest car between car1 and car4. Store result in a variable
+  called fastest1.
+- Step two: call getFastestCar to determine fastest car between car3 and car4. Store result in a variable
+  called fastest2.
+- Step three: call getFastestCar to determine the winner between fastest1 and fastest2. Store result in a
+  variable called ultimateFastestCar.
+- Return the ultimateFastestCar object at the end.
+*/
+function race() {
+  // Part 5 - Define your winner1 and winner2 variables
+
+
+
+
+  // Part 5 - Define your finalWinner variable
+
+
+
+  // Part 5 - Return the final winner
+
+
+}
+
