a3c6085947cfcf4a3076fe7d61e4c66a02ad6224
Alex Aaron <alexfrancisaaron@gmail.com>
2026-06-23 16:35:11 +0000

    created 2d array practice

---
+++ 2d-array-practice/nested-array-reassignment.js
@@ -0,0 +1,27 @@
+/*
+Create a function called reassignNegatives that takes in a 2D array of numbers. 
+This function should use nested for loops to access each number in the 2D array.
+If the current number is negative, the function should reassign that index to 0.
+If the number is not negative, the function should leave the value unchanged. The
+function should return the array at the end.
+*/
+
+function reassignNegatives(array){
+
+}
+
+var exampleArray = [
+  [-2, 4, 6],
+  [8, -6, 12]
+];
+
+var result = reassignNegatives(exampleArray);
+
+// console.log(result);
+/*
+SHOULD RETURN => 
+[
+  [0, 4, 6],
+  [8, 0, 12]
+]
+*/
---
+++ 2d-array-practice/nested-array-sum.js
@@ -0,0 +1,18 @@
+/*
+Create a function called getSum that takes in a 2D array of numbers. This function
+should utilize nested for loops to iterate over every item in the 2D array. The function
+should return the sum of all the numbers that are greater than zero.
+*/
+
+function getSum(array){
+  
+}
+
+var exampleArray = [
+  [-1, 1, 10],
+  [5, -5, 6]
+];
+
+var result = getSum(exampleArray);
+
+// console.log(result); // answer should be 22
